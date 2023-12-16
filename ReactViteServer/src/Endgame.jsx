25be4df96adaa91c92f47cf47333317f5d097ee0
StepanLebedevu <stepan_leb@mail.ru>
2023-12-17 00:26:02 +0300

    sadasd

--- ReactViteServer/src/Endgame.jsx
+++ ReactViteServer/src/Endgame.jsx
@@ -1,7 +1,14 @@
 /* eslint-disable react/prop-types */
 
+import { useState } from "react"
 
-const Endgame = ({scores}) => {
+
+const Endgame = ({scores, connected}) => {
+  const modifiedObj = (object) => {
+    return {userId: object.userId, userName: connected.filter(a => a.userId === obj.userId)[0].name, scores: object.scores}
+  }
+  scores.map((obj) => modifiedObj(obj))
+
   return (
     <div className="game_geometry white_bg">
         Endgame! Hooray!
